1a2d56a758e88090c7959959672e8a4b50a02567
SamuelEli <Kartelsamuel1997@gmail.com>
2018-04-20 06:53:11 -0700

    my first react app

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {Carousel} from './images'
+import {imgUrls} from './images'
+class App extends Component {
+  render() {
+    console.log('');
+    return (
+      <div className="App">
+      <Carousel/>
+      </div>
+    );
+  }
+}
+export default App;
